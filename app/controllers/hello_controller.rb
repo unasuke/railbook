3812e7fb05313cb1237fc5b4a142a8c2f3878007
Kohei Makita <nikuzuki29@gmail.com>
2015-08-18 03:29:40 +0900

    output "こんにちは、nikuzuki"

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,2 +1,5 @@
 class HelloController < ApplicationController
+  def index
+    render text: 'こんにちは、nikuzuki!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+Railbook::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  match ':controller(/:action(/:id))', via: [ :get, :post, :patch ]
 end
